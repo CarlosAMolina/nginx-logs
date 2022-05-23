386d1919278e4ceb52820edb97edfaeecef9cc3b
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-05-24 00:00:30 +0200

    Write errors to stderr

--- rust/nginx_logs/src/main.rs
+++ rust/nginx_logs/src/main.rs
@@ -5,11 +5,11 @@ use nginx_logs::Config;
 
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     if let Err(e) = nginx_logs::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
