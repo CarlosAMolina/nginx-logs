221fcfb43a59f98b9579846e7262b1cffa2692ee
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-05-29 15:25:44 +0200

    Add parse lines as Log struct

--- rust/nginx_logs/src/lib.rs
+++ rust/nginx_logs/src/lib.rs
@@ -1,9 +1,12 @@
 use std::env;
 use std::error::Error;
-
+use std::fmt;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+
+use lazy_static::lazy_static;
+use regex::Regex;
 
 pub struct Config {
     filename: String,
@@ -23,54 +26,113 @@ impl Config {
     }
 }
 
+struct Log<'a> {
+    remote_addr: &'a str,
+    remote_user: &'a str,
+    time_local: &'a str,
+    request: &'a str,
+    status: &'a str,
+    body_bytes_sent: &'a str,
+    http_referer: &'a str,
+    http_user_agent: &'a str,
+}
+
+impl<'a> fmt::Display for Log<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{},{},{},{},{},{},{},{}",
+            self.remote_addr,
+            self.remote_user,
+            self.time_local,
+            self.request,
+            self.status,
+            self.body_bytes_sent,
+            self.http_referer,
+            self.http_user_agent,
+        )
+    }
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let lines = read_lines(config.filename).expect("Something went wrong reading the file");
-    for line in lines {
-        let log = line.expect("Something went wrong reading the line");
-        println!("{}", log);
+    lazy_static! {
+        static ref RE: Regex = Regex::new(
+            r#"(?x)
+          (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) # IPv4
+          \s-\s
+          (.+)                                 # Remote user
+          \s\[
+          (.+)                                 # Time local
+          \]\s"
+          (.+)                                 # Request
+          "\s
+          (\d{1,3})                            # Status
+          \s
+          (\d+)                                # Body bytes sent
+          \s"
+          (.+)                                 # HTTP referer
+          "\s"
+          (.+)                                 # HTTP user agent
+          "
+        "#,
+        )
+        .unwrap();
     }
-    //println!("Matches:");
-    //// TODO
-    //for line in contents.lines() {
-    //    println!("Checking: {}", line);
-    //    if !line.contains("111") {
-    //        eprintln!("Not parsed {}", line);
-    //    }
-    //}
-    //for line in search("111", &contents) {
-    //    println!("{}", line);
-    //}
-
-    //println!("File content:\n{}", contents);
+    for line in lines {
+        let log_line = line.expect("Something went wrong reading the line");
+        println!("{}", log_line);
+        let log = get_log(&log_line, &RE).map(|m| m.to_string());
+        match log {
+            None => { println!("- Not parsed")},
+            Some(log_parsed) => { println!("- Parsed: {}", log_parsed)},
+        }
+    }
     Ok(())
 }
 
 // https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents
-        .lines()
-        .filter(|line| line.contains(query))
-        .collect()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn one_result() {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.";
-
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
+fn get_log<'a>(text: &'a str, re: &'a Regex) -> Option<Log<'a>> {
+    re.captures(text).and_then(|cap| {
+        let groups = (
+            cap.get(1),
+            cap.get(2),
+            cap.get(3),
+            cap.get(4),
+            cap.get(5),
+            cap.get(6),
+            cap.get(7),
+            cap.get(8),
+        );
+        match groups {
+            (
+                Some(remote_addr),
+                Some(remote_user),
+                Some(time_local),
+                Some(request),
+                Some(status),
+                Some(body_bytes_sent),
+                Some(http_referer),
+                Some(http_user_agent),
+            ) => Some(Log {
+                remote_addr: remote_addr.as_str(),
+                remote_user: remote_user.as_str(),
+                time_local: time_local.as_str(),
+                request: request.as_str(),
+                status: status.as_str(),
+                body_bytes_sent: body_bytes_sent.as_str(),
+                http_referer: http_referer.as_str(),
+                http_user_agent: http_user_agent.as_str(),
+            }),
+            _ => None,
+        }
+    })
 }
