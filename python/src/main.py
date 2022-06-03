7f7176c703433513289442605e47cff480e5a912
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-06-03 22:47:33 +0200

    Add python parse args

--- python/src/main.py
+++ python/src/main.py
@@ -1,3 +1,12 @@
-if __name__ == "__main__":
+import argparse
 
-    print("hi")
+if __name__ == "__main__":
+    # https://docs.python.org/3/library/argparse.html#the-add-argument-method
+    parser = argparse.ArgumentParser(description="Export Nginx logs to a csv file.")
+    parser.add_argument(
+        "path",
+        type=str,
+        help="path to a folder with the log files or to an specific file",
+    )
+    args = parser.parse_args()
+    print(f"Checking: {args.path}")
