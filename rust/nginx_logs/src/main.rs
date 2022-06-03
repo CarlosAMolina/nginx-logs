c025f9f3298cd29b7a151b6627904e8f2211e4a6
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-06-03 05:46:54 +0200

    Add print time elapsed by the program

--- rust/nginx_logs/src/main.rs
+++ rust/nginx_logs/src/main.rs
@@ -1,9 +1,12 @@
 use std::env;
 use std::process;
+use std::time::Instant;
 
 use nginx_logs::Config;
 
 fn main() {
+    //https://rust-lang-nursery.github.io/rust-cookbook/datetime/duration.html#measure-the-elapsed-time-between-two-code-sections
+    let start = Instant::now();
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
@@ -12,4 +15,6 @@ fn main() {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
+    let duration = start.elapsed();
+    println!("Time elapsed: {:?}", duration);
 }
