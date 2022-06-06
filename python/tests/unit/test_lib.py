a1a05d4d404196469dec3efb5d0fb10eee20635c
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-06-06 23:36:09 +0200

    Test python get_log for not parsed log

--- python/tests/unit/test_lib.py
+++ python/tests/unit/test_lib.py
@@ -1,17 +1,24 @@
-import datetime
 import unittest
 
 from src import lib
 
 
 class TestFunctions(unittest.TestCase):
-    def test_get_log(self):
+    def test_get_log_for_parsed_log(self):
         line = (
             '8.8.8.8 - abc [28/Nov/2021:00:18:22 +0100] "GET / HTTP/1.1" 200 77 "-"'
             ' "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36'
             ' (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"'
         )
         self.assertIsNotNone(lib.get_log(line))
+
+    def test_get_log_for_not_parsed_log(self):
+        line = (
+            '8.8.8.8 - abc [28/Nov/2021:00:18:22 +0100 "GET / HTTP/1.1" 200 77 "-"'
+            ' "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36'
+            ' (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"'
+        )
+        self.assertIsNone(lib.get_log(line))
 
 
 if __name__ == "__main__":
