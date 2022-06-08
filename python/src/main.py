4f75206ff2874cfa237343feac7a8952a6aaa43d
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-06-08 07:34:15 +0200

    Add python time elapsed

--- python/src/main.py
+++ python/src/main.py
@@ -1,5 +1,12 @@
 import lib
 
+# https://stackoverflow.com/questions/7370801/how-to-measure-elapsed-time-in-python#7370824
+from timeit import default_timer as timer
+
+
 if __name__ == "__main__":
+    start = timer()
     args = lib.get_args_parsed()
     lib.run(args)
+    end = timer()
+    print(f"Time elapsed: {end - start}")
