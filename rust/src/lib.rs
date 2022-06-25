cd7999da7091390381e2f6dc70a8740f5b51e8e2
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-06-25 13:28:21 +0200

    Refactor create mod log

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,5 @@
 use std::env;
 use std::error::Error;
-use std::fmt;
 use std::fs::{self, File};
 use std::io::Write;
 use std::io::{self, BufRead};
@@ -10,7 +9,6 @@ use csv::Writer;
 use csv::WriterBuilder;
 use lazy_static::lazy_static;
 use regex::Regex;
-use serde_derive::Serialize;
 
 pub struct Config {
     file_or_path: String,
@@ -30,34 +28,106 @@ impl Config {
     }
 }
 
-#[derive(Debug, PartialEq, Serialize)]
-struct Log<'a> {
-    remote_addr: &'a str,
-    remote_user: &'a str,
-    time_local: &'a str,
-    request: &'a str,
-    status: &'a str,
-    body_bytes_sent: &'a str,
-    http_referer: &'a str,
-    http_user_agent: &'a str,
+mod log {
+    use std::fmt;
+
+    use lazy_static::lazy_static;
+    use regex::Regex;
+    use serde_derive::Serialize;
+
+    #[derive(Debug, PartialEq, Serialize)]
+    pub struct Log<'a> {
+        pub remote_addr: &'a str,
+        pub remote_user: &'a str,
+        pub time_local: &'a str,
+        pub request: &'a str,
+        pub status: &'a str,
+        pub body_bytes_sent: &'a str,
+        pub http_referer: &'a str,
+        pub http_user_agent: &'a str,
+    }
+
+    impl<'a> fmt::Display for Log<'a> {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(
+                f,
+                "{},{},{},{},{},{},{},{}",
+                self.remote_addr,
+                self.remote_user,
+                self.time_local,
+                self.request,
+                self.status,
+                self.body_bytes_sent,
+                self.http_referer,
+                self.http_user_agent,
+            )
+        }
+    }
+
+    pub fn get_log(text: &str) -> Option<Log> {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(
+                r#"(?x)
+              ^
+              ((\d{1,3}[\.]){3}\d{1,3}) # IPv4
+              \s-\s
+              (.+)                      # Remote user
+              \s\[
+              (.+)                      # Time local
+              \]\s"
+              (.*)                      # Request
+              "\s
+              (\d{1,3})                 # Status
+              \s
+              (\d+)                     # Body bytes sent
+              \s"
+              (.+)                      # HTTP referer
+              "\s"
+              (.+)                      # HTTP user agent
+              "
+            "#,
+            )
+            .unwrap();
+        }
+        RE.captures(text).and_then(|cap| {
+            let groups = (
+                cap.get(1),
+                cap.get(3),
+                cap.get(4),
+                cap.get(5),
+                cap.get(6),
+                cap.get(7),
+                cap.get(8),
+                cap.get(9),
+            );
+            match groups {
+                (
+                    Some(remote_addr),
+                    Some(remote_user),
+                    Some(time_local),
+                    Some(request),
+                    Some(status),
+                    Some(body_bytes_sent),
+                    Some(http_referer),
+                    Some(http_user_agent),
+                ) => Some(Log {
+                    remote_addr: remote_addr.as_str(),
+                    remote_user: remote_user.as_str(),
+                    time_local: time_local.as_str(),
+                    request: request.as_str(),
+                    status: status.as_str(),
+                    body_bytes_sent: body_bytes_sent.as_str(),
+                    http_referer: http_referer.as_str(),
+                    http_user_agent: http_user_agent.as_str(),
+                }),
+                _ => None,
+            }
+        })
+    }
+
 }
 
-impl<'a> fmt::Display for Log<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{},{},{},{},{},{},{},{}",
-            self.remote_addr,
-            self.remote_user,
-            self.time_local,
-            self.request,
-            self.status,
-            self.body_bytes_sent,
-            self.http_referer,
-            self.http_user_agent,
-        )
-    }
-}
+use crate::log as m_log;
 
 struct FileAndDisplay<'a> {
     display: &'a std::path::Display<'a>,
@@ -151,7 +221,7 @@ fn export_file_to_csv(
     let lines = read_lines(file_to_check).expect("Something went wrong reading the file");
     for line in lines {
         let log_line = line.expect("Something went wrong reading the line");
-        let log = get_log(&log_line);
+        let log = m_log::get_log(&log_line);
         match log {
             None => {
                 eprintln!("Not parsed: {}", log_line);
@@ -196,71 +266,12 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn get_log(text: &str) -> Option<Log> {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(
-            r#"(?x)
-          ^
-          ((\d{1,3}[\.]){3}\d{1,3}) # IPv4
-          \s-\s
-          (.+)                      # Remote user
-          \s\[
-          (.+)                      # Time local
-          \]\s"
-          (.*)                      # Request
-          "\s
-          (\d{1,3})                 # Status
-          \s
-          (\d+)                     # Body bytes sent
-          \s"
-          (.+)                      # HTTP referer
-          "\s"
-          (.+)                      # HTTP user agent
-          "
-          $
-        "#,
-        )
-        .unwrap();
-    }
-    RE.captures(text).and_then(|cap| {
-        let groups = (
-            cap.get(1),
-            cap.get(3),
-            cap.get(4),
-            cap.get(5),
-            cap.get(6),
-            cap.get(7),
-            cap.get(8),
-            cap.get(9),
-        );
-        match groups {
-            (
-                Some(remote_addr),
-                Some(remote_user),
-                Some(time_local),
-                Some(request),
-                Some(status),
-                Some(body_bytes_sent),
-                Some(http_referer),
-                Some(http_user_agent),
-            ) => Some(Log {
-                remote_addr: remote_addr.as_str(),
-                remote_user: remote_user.as_str(),
-                time_local: time_local.as_str(),
-                request: request.as_str(),
-                status: status.as_str(),
-                body_bytes_sent: body_bytes_sent.as_str(),
-                http_referer: http_referer.as_str(),
-                http_user_agent: http_user_agent.as_str(),
-            }),
-            _ => None,
-        }
-    })
-}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::log::get_log;
+    use crate::log::Log;
     #[test]
     fn test_get_log_filenames_sort_reverse() {
         let filenames = vec![
