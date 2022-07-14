84d8ce8e7d9e775e9369fc4293787f97ba722925
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-07-14 21:07:27 +0200

    Refactor rename code in lib.rs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,36 +8,36 @@ mod read_file;
 mod write_file;
 
 pub struct Config {
-    file_or_path: String,
+    pathname: String,
 }
 
 impl Config {
     pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
         //println!("Arguments: {:?}", args);
         args.next();
-        let file_or_path = match args.next() {
+        let pathname = match args.next() {
             Some(arg) => arg,
-            None => return Err("Didn't get a file name or a path"),
+            None => return Err("No pathname provided"),
         };
-        println!("Checking: {}", file_or_path);
+        println!("Checking: {}", pathname);
 
-        Ok(Config { file_or_path })
+        Ok(Config { pathname })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let (mut writer_csv, mut writer_error) = create_file::get_result_writers(&config.file_or_path)?;
-    for pathname in filter_file::get_pathnames_to_analyze(&config.file_or_path)? {
+    let (mut writer_csv, mut writer_error) = create_file::get_result_writers(&config.pathname)?;
+    for pathname in filter_file::get_pathnames_to_analyze(&config.pathname)? {
         //for line in reader.lines() {
         for line in read_file::get_lines_in_file(&pathname) {
             let line_str = line.expect("Something went wrong reading the line");
             let log = m_log::get_log(&line_str);
             match log {
-                None => {
-                    write_file::write_to_file_error(line_str, &mut writer_error)?;
-                }
                 Some(log_csv) => {
                     write_file::write_to_file_result(log_csv, &mut writer_csv)?;
+                }
+                None => {
+                    write_file::write_to_file_error(line_str, &mut writer_error)?;
                 }
             }
         }
