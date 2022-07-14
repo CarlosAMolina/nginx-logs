7e4535e32b8984075041655c871aa5d31ab39cf6
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-07-14 21:15:19 +0200

    Refactor rename code in write_file.rs

--- rust/src/write_file.rs
+++ rust/src/write_file.rs
@@ -7,15 +7,15 @@ use crate::m_log::Log;
 
 pub fn write_to_file_error(
     line: String,
-    file_error: &mut BufWriter<File>,
+    writer: &mut BufWriter<File>,
 ) -> Result<(), Box<dyn Error>> {
     eprintln!("Not parsed: {}", line);
-    write_line_to_file(file_error, line)?;
+    write_line_to_file(line, writer)?;
     Ok(())
 }
 
-fn write_line_to_file(file_error: &mut BufWriter<File>, line: String) -> Result<(), String> {
-    if let Err(e) = file_error.write_all(format!("{}\n", line).as_bytes()) {
+fn write_line_to_file(line: String, writer: &mut BufWriter<File>) -> Result<(), String> {
+    if let Err(e) = writer.write_all(format!("{}\n", line).as_bytes()) {
         return Err(e.to_string());
     }
     Ok(())
@@ -23,8 +23,8 @@ fn write_line_to_file(file_error: &mut BufWriter<File>, line: String) -> Result<
 
 pub fn write_to_file_result(
     line: Log,
-    writer_csv: &mut Writer<File>,
+    writer: &mut Writer<File>,
 ) -> Result<(), Box<dyn Error>> {
-    writer_csv.serialize(line)?;
+    writer.serialize(line)?;
     Ok(())
 }
