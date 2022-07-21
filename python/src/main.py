896c3636e091e0cb768cfa573cc31fa3e7fe1a9f
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-07-21 21:26:35 +0200

    Improve python show duration

--- python/src/main.py
+++ python/src/main.py
@@ -3,6 +3,12 @@ from timeit import default_timer as timer
 
 # https://stackoverflow.com/questions/7370801/how-to-measure-elapsed-time-in-python#7370824
 
+def print_duration(duration: float):
+    unit_of_time = "s"
+    if duration < 1:
+        unit_of_time = "ms"
+        duration = duration * 1000
+    print(f"Time elapsed: {duration}{unit_of_time}")
 
 if __name__ == "__main__":
     start = timer()
@@ -10,5 +16,4 @@ if __name__ == "__main__":
     lib.run(args)
     end = timer()
     duration = end - start
-    # print(f"Time elapsed: {duration}s")
-    print(f"Time elapsed: {duration * 1000}ms")
+    print_duration(duration)
