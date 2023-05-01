2ec3bbc6508959c91c8cefb1620faf0bae25f2cb
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-05-01 22:46:26 +0200

    Partial commit calc average

--- measure/plot/src/transformers/execution_time.py
+++ measure/plot/src/transformers/execution_time.py
@@ -3,5 +3,13 @@ import pandas as pd
 
 class DfToPlot:
     def __call__(self, df: pd.DataFrame) -> pd.DataFrame:
-        return df
+        result = df
+        result = self._get_df_set_average(result)
+        return result
+
+    def _get_df_set_average(self, df: pd.DataFrame) -> pd.DataFrame:
+        result = df
+        result["average_s"] = result.mean(axis='columns', numeric_only=True)
+        breakpoint()
+        return result
 
